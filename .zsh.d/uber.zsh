406d5a60e5b18e93453f535372a40fc00c0faa91
Alex Bahouth <abahouth@uber.com>
2014-12-10 16:14:22 -0800

    Updated uber local for mac

--- .zsh.d/uber.zsh
+++ .zsh.d/uber.zsh
@@ -2,4 +2,4 @@ export UBER_HOME=~/Uber
 
 [[ -e /usr/local/bin/virtualenvwrapper.sh ]] && source /usr/local/bin/virtualenvwrapper.sh
 
-alias vsql="LANG=en.UTF-8 /opt/vertica/bin/vsql"
+alias vsql="LANG=en_US.UTF-8 /opt/vertica/bin/vsql"
